117551bc332634852bef69e5db48b3d6a734aa82
rideranton <rideranton@hotmail.com>
2023-07-24 00:30:01 -0700

    Add ImageDto changes

--- src/DTO/ImageDto.php
+++ src/DTO/ImageDto.php
@@ -4,24 +4,54 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
-class ImageDto
+use App\Entity\Image;
+use OpenApi\Attributes as OA;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[OA\Schema()]
+class ImageDto implements \JsonSerializable
 {
+    #[Groups(['common'])]
     public ?string $filePath = null;
+    #[Groups(['common'])]
+    public ?string $sourceUrl = null;
+    #[Groups(['common'])]
+    public ?string $altText = null;
+    #[Groups(['common'])]
     public ?int $width = null;
+    #[Groups(['common'])]
     public ?int $height = null;
 
-    public function create(string $filePath, int $width = null, int $height = null, int $id = null): self
+    public function __construct(Image $image = null)
     {
-        $this->id = $id;
+        if (null !== $image) {
+            $this->filePath = $image->filePath;
+            $this->sourceUrl = $image->sourceUrl;
+            $this->altText = $image->altText;
+            $this->width = $image->width;
+            $this->height = $image->height;
+        }
+    }
+
+    public function create(string $filePath, int $width = null, int $height = null, string $altText = null, string $sourceUrl = null): self
+    {
         $this->filePath = $filePath;
+        $this->altText = $altText;
         $this->width = $width;
         $this->height = $height;
+        $this->sourceUrl = $sourceUrl;
 
         return $this;
     }
 
-    public function getId(): ?int
+    public function jsonSerialize(): mixed
     {
-        return $this->id;
+        return [
+            'filePath' => $this->filePath,
+            'sourceUrl' => $this->sourceUrl,
+            'altText' => $this->altText,
+            'width' => $this->width,
+            'height' => $this->height,
+        ];
     }
 }
