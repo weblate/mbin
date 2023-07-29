18a0261b1ae9a95900e1addeae52fe9962f0a089
rideranton <rideranton@hotmail.com>
2023-07-28 23:39:42 -0700

    Add API to retrieve current user's followed/followers

--- src/Controller/Api/User/UserRetrieveApi.php
+++ src/Controller/Api/User/UserRetrieveApi.php
@@ -501,6 +501,179 @@ class UserRetrieveApi extends UserBaseApi
 
     #[OA\Response(
         response: 200,
+        description: 'Returns a paginated list of users being followed by the current user',
+        content: new OA\JsonContent(
+            type: 'object',
+            properties: [
+                new OA\Property(
+                    property: 'items',
+                    type: 'array',
+                    items: new OA\Items(ref: new Model(type: UserResponseDto::class))
+                ),
+                new OA\Property(
+                    property: 'pagination',
+                    ref: new Model(type: PaginationSchema::class)
+                ),
+            ]
+        ),
+        headers: [
+            new OA\Header(header: 'X-RateLimit-Remaining', schema: new OA\Schema(type: 'integer'), description: 'Number of requests left until you will be rate limited'),
+            new OA\Header(header: 'X-RateLimit-Retry-After', schema: new OA\Schema(type: 'integer'), description: 'Unix timestamp to retry the request after'),
+            new OA\Header(header: 'X-RateLimit-Limit', schema: new OA\Schema(type: 'integer'), description: 'Number of requests available'),
+        ]
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Permission denied due to missing or expired token',
+        content: new OA\JsonContent(ref: new Model(type: \App\Schema\Errors\UnauthorizedErrorSchema::class))
+    )]
+    #[OA\Response(
+        response: 403,
+        description: 'This user does not allow others to view the users they follow',
+        content: new OA\JsonContent(ref: new Model(type: \App\Schema\Errors\ForbiddenErrorSchema::class))
+    )]
+    #[OA\Response(
+        response: 429,
+        description: 'You are being rate limited',
+        content: new OA\JsonContent(ref: new Model(type: \App\Schema\Errors\TooManyRequestsErrorSchema::class)),
+        headers: [
+            new OA\Header(header: 'X-RateLimit-Remaining', schema: new OA\Schema(type: 'integer'), description: 'Number of requests left until you will be rate limited'),
+            new OA\Header(header: 'X-RateLimit-Retry-After', schema: new OA\Schema(type: 'integer'), description: 'Unix timestamp to retry the request after'),
+            new OA\Header(header: 'X-RateLimit-Limit', schema: new OA\Schema(type: 'integer'), description: 'Number of requests available'),
+        ]
+    )]
+    #[OA\Parameter(
+        name: 'p',
+        description: 'Page of users to retrieve',
+        in: 'query',
+        schema: new OA\Schema(type: 'integer', default: 1, minimum: 1)
+    )]
+    #[OA\Parameter(
+        name: 'perPage',
+        description: 'Number of users per page',
+        in: 'query',
+        schema: new OA\Schema(
+            type: 'integer',
+            default: UserRepository::PER_PAGE,
+            minimum: self::MIN_PER_PAGE,
+            maximum: self::MAX_PER_PAGE
+        )
+    )]
+    #[OA\Tag(name: 'user')]
+    #[Security(name: 'oauth2', scopes: ['user:follow'])]
+    #[IsGranted('ROLE_OAUTH2_USER:FOLLOW')]
+    public function followedByCurrent(
+        UserRepository $repository,
+        UserFactory $factory,
+        Request $request,
+        RateLimiterFactory $apiReadLimiter,
+    ): JsonResponse {
+        $headers = $this->rateLimit($apiReadLimiter);
+
+        $users = $repository->findFollowing(
+            $this->getPageNb($request),
+            $this->getUserOrThrow(),
+            self::constrainPerPage($request->get('perPage', UserRepository::PER_PAGE))
+        );
+
+        $dtos = [];
+        foreach ($users->getCurrentPageResults() as $value) {
+            assert($value instanceof UserFollow);
+            array_push($dtos, $this->serializeUser($factory->createDto($value->following)));
+        }
+
+        return new JsonResponse(
+            $this->serializePaginated($dtos, $users),
+            headers: $headers
+        );
+    }
+
+    #[OA\Response(
+        response: 200,
+        description: 'Returns a paginated list of users following the current user',
+        content: new OA\JsonContent(
+            type: 'object',
+            properties: [
+                new OA\Property(
+                    property: 'items',
+                    type: 'array',
+                    items: new OA\Items(ref: new Model(type: UserResponseDto::class))
+                ),
+                new OA\Property(
+                    property: 'pagination',
+                    ref: new Model(type: PaginationSchema::class)
+                ),
+            ]
+        ),
+        headers: [
+            new OA\Header(header: 'X-RateLimit-Remaining', schema: new OA\Schema(type: 'integer'), description: 'Number of requests left until you will be rate limited'),
+            new OA\Header(header: 'X-RateLimit-Retry-After', schema: new OA\Schema(type: 'integer'), description: 'Unix timestamp to retry the request after'),
+            new OA\Header(header: 'X-RateLimit-Limit', schema: new OA\Schema(type: 'integer'), description: 'Number of requests available'),
+        ]
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Permission denied due to missing or expired token',
+        content: new OA\JsonContent(ref: new Model(type: \App\Schema\Errors\UnauthorizedErrorSchema::class))
+    )]
+    #[OA\Response(
+        response: 429,
+        description: 'You are being rate limited',
+        content: new OA\JsonContent(ref: new Model(type: \App\Schema\Errors\TooManyRequestsErrorSchema::class)),
+        headers: [
+            new OA\Header(header: 'X-RateLimit-Remaining', schema: new OA\Schema(type: 'integer'), description: 'Number of requests left until you will be rate limited'),
+            new OA\Header(header: 'X-RateLimit-Retry-After', schema: new OA\Schema(type: 'integer'), description: 'Unix timestamp to retry the request after'),
+            new OA\Header(header: 'X-RateLimit-Limit', schema: new OA\Schema(type: 'integer'), description: 'Number of requests available'),
+        ]
+    )]
+    #[OA\Parameter(
+        name: 'p',
+        description: 'Page of users to retrieve',
+        in: 'query',
+        schema: new OA\Schema(type: 'integer', default: 1, minimum: 1)
+    )]
+    #[OA\Parameter(
+        name: 'perPage',
+        description: 'Number of users per page',
+        in: 'query',
+        schema: new OA\Schema(
+            type: 'integer',
+            default: UserRepository::PER_PAGE,
+            minimum: self::MIN_PER_PAGE,
+            maximum: self::MAX_PER_PAGE
+        )
+    )]
+    #[OA\Tag(name: 'user')]
+    #[Security(name: 'oauth2', scopes: ['user:follow'])]
+    #[IsGranted('ROLE_OAUTH2_USER:FOLLOW')]
+    public function followersOfCurrent(
+        UserRepository $repository,
+        UserFactory $factory,
+        Request $request,
+        RateLimiterFactory $apiReadLimiter,
+    ): JsonResponse {
+        $headers = $this->rateLimit($apiReadLimiter);
+
+        $users = $repository->findFollowers(
+            $this->getPageNb($request),
+            $this->getUserOrThrow(),
+            self::constrainPerPage($request->get('perPage', UserRepository::PER_PAGE))
+        );
+
+        $dtos = [];
+        foreach ($users->getCurrentPageResults() as $value) {
+            assert($value instanceof UserFollow);
+            array_push($dtos, $this->serializeUser($factory->createDto($value->follower)));
+        }
+
+        return new JsonResponse(
+            $this->serializePaginated($dtos, $users),
+            headers: $headers
+        );
+    }
+
+    #[OA\Response(
+        response: 200,
         description: 'Returns a paginated list of users blocked by the current user',
         content: new OA\JsonContent(
             type: 'object',
